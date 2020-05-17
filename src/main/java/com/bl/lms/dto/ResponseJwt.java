429b0c8963820cab2eaf3a46f46a949d47ddbdcb
unknown <revitekale1910@gmail.com>
2020-05-17 13:22:52 +0530

    ADDED:Class to send response

---
+++ src/main/java/com/bl/lms/dto/ResponseJwt.java
@@ -0,0 +1,16 @@
+package com.bl.lms.dto;
+
+import java.io.Serializable;
+
+public class ResponseJwt implements Serializable {
+    private static final long serialVersionUID = -8091879091924046844L;
+    private final String jwttoken;
+
+    public ResponseJwt(String jwttoken) {
+        this.jwttoken = jwttoken;
+    }
+
+    public String getToken() {
+        return this.jwttoken;
+    }
+}
