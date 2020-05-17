86c556bbd8eaea0194d7b846985b89b801402c1a
unknown <revitekale1910@gmail.com>
2020-05-17 22:58:32 +0530

    MODIFIED:Cladd to add findByMail m ethod

--- src/main/java/com/bl/lms/repository/UserRepository.java
+++ src/main/java/com/bl/lms/repository/UserRepository.java
@@ -9,5 +9,8 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     @Query("select u from user_details u where u.first_name = ?1")
-    public User findByFirstName(String first_name);
+    User findByFirstName(String first_name);
+
+    @Query("select u from user_details u where u.email = ?1")
+    User findByEmail(String email);
 }
