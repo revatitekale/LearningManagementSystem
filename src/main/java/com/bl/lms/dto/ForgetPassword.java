e7669961282d9bb88f7e50face7ea8a6b049935e
unknown <revitekale1910@gmail.com>
2020-05-17 22:56:48 +0530

    ADDED:Class for Forget password dto

---
+++ src/main/java/com/bl/lms/dto/ForgetPassword.java
@@ -0,0 +1,14 @@
+package com.bl.lms.dto;
+
+public class ForgetPassword {
+
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
