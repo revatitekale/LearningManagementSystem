cf029b6df96def269414fdaa81c5ad698214f6f4
unknown <revitekale1910@gmail.com>
2020-05-17 13:48:01 +0530

    ADDED:LoginAPI

---
+++ src/test/java/com/bl/lms/LearningManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bl.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
