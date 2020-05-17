c359448dcc22ea297c3c482da40d91b730f4ebc8
unknown <revitekale1910@gmail.com>
2020-05-17 13:14:28 +0530

    ADDED:Login response when user successfully registered

---
+++ src/main/java/com/bl/lms/dto/LoginResponse.java
@@ -0,0 +1,28 @@
+package com.bl.lms.dto;
+
+public class LoginResponse {
+    int status;
+    String message;
+
+    public LoginResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    //Getter and Setter
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
