25f9f0246d554a39bf5a8fc3ec82cd883cd88fb6
unknown <revitekale1910@gmail.com>
2020-05-28 01:33:59 +0530

    MODIFIED:Used lombok features

--- src/main/java/com/bl/lms/dto/ResponseJwt.java
+++ src/main/java/com/bl/lms/dto/ResponseJwt.java
@@ -8,6 +8,6 @@ import java.io.Serializable;
 public class ResponseJwt implements Serializable {
 
     private static final long serialVersionUID = -8091879091924046844L;
-    private final String jwttoken;
+    private final String jwtToken;
 
 }
