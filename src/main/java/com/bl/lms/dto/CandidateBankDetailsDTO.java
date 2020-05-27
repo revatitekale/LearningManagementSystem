fe3ee32c9ace4a56c747c84bc1d327d207fcff19
unknown <revitekale1910@gmail.com>
2020-05-28 01:28:38 +0530

    ADDED:Bank details of candidate

---
+++ src/main/java/com/bl/lms/dto/CandidateBankDetailsDTO.java
@@ -0,0 +1,25 @@
+package com.bl.lms.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Data
+public class CandidateBankDetailsDTO {
+    @NotNull
+    private long id;
+    @NotNull
+    private long candidateId;
+    private String name;
+    private long accountNumber;
+    private String isAccountNumberVerified;
+    private String ifscCode;
+    private String isIfscCodeVerified;
+    private String panNumber;
+    private String isPanNumberVerified;
+    private long addhaarNumber;
+    private String isAdhaarNumVerified;
+    private Date creatorStamp;
+    private String creatorUser;
+}
