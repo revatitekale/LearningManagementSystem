54c47f69d2059c065b7960a57a60893aa41a622b
unknown <revitekale1910@gmail.com>
2020-05-28 01:32:06 +0530

    MODIFIED:Used lombok features

--- src/main/java/com/bl/lms/dto/Response.java
+++ src/main/java/com/bl/lms/dto/Response.java
@@ -2,10 +2,9 @@ package com.bl.lms.dto;
 import lombok.Data;
 
 @Data
-
 public class Response {
-    int status;
-    String message;
+    public Object status;
+    public String message;
 
     public Response(int status, String message) {
         this.status = status;
