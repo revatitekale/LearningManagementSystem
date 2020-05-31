c49dd93f0a64ed118cce04b40dd3e7461d554fc7
unknown <revitekale1910@gmail.com>
2020-05-31 20:38:50 +0530

    REFACTORED:Removed unneccessary controller and added in dingle controller

--- src/main/java/com/bl/lms/controller/FellowshipCandidateController.java
+++ src/main/java/com/bl/lms/controller/FellowshipCandidateController.java
@@ -1,5 +1,7 @@
 package com.bl.lms.controller;
 
+import com.bl.lms.dto.CandidateBankDetailsDTO;
+import com.bl.lms.dto.CandidateQualificationDTO;
 import com.bl.lms.dto.FellowshipCandidateDTO;
 import com.bl.lms.dto.Response;
 import com.bl.lms.service.IFellowshipCandidateService;
@@ -22,6 +24,16 @@ public class FellowshipCandidateController {
     @GetMapping("/getcount")
     public ResponseEntity<Response> getCandidateCount() {
         return ResponseEntity.ok(fellowshipCandidateService.getCandidateCount());
+    }
+
+    @PostMapping("/updatequalificationdetails")
+    public ResponseEntity<Response> updateCandidateQualificationInfo (@RequestBody CandidateQualificationDTO candidateQualificationDTO) {
+        return ResponseEntity.ok(fellowshipCandidateService.updateQualificationDetails(candidateQualificationDTO));
+    }
+
+    @PostMapping("/updatebankdetails")
+    public ResponseEntity<Response> updateCandidateBankInfo(@RequestBody CandidateBankDetailsDTO candidateBankDetailsDTO) {
+        return ResponseEntity.ok(fellowshipCandidateService.updateCandidateBankInfo(candidateBankDetailsDTO));
     }
 
 }
