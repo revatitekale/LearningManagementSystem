ecd216fcfb9ef25399d39069c527b418b6ace7ce
unknown <revitekale1910@gmail.com>
2020-05-31 20:49:20 +0530

    MODIFIED:methods of bank details n qualification details

--- src/main/java/com/bl/lms/service/FellowshipCandidateServiceImpl.java
+++ src/main/java/com/bl/lms/service/FellowshipCandidateServiceImpl.java
@@ -1,16 +1,23 @@
 package com.bl.lms.service;
 
+import com.bl.lms.dto.CandidateBankDetailsDTO;
+import com.bl.lms.dto.CandidateQualificationDTO;
 import com.bl.lms.dto.FellowshipCandidateDTO;
 import com.bl.lms.dto.Response;
 import com.bl.lms.model.Candidate;
+import com.bl.lms.model.CandidateBankDetails;
+import com.bl.lms.model.CandidateQualification;
 import com.bl.lms.model.FellowshipCandidate;
+import com.bl.lms.repository.BankDetailsRepository;
 import com.bl.lms.repository.CandidateRepository;
 import com.bl.lms.repository.FellowshipCandidateRepository;
+import com.bl.lms.repository.QualificationRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 
 @Service
@@ -21,6 +28,12 @@ public class FellowshipCandidateServiceImpl implements IFellowshipCandidateServi
 
     @Autowired
     private CandidateRepository hiredCandidateRepository;
+
+    @Autowired
+    private BankDetailsRepository bankDetailsRepository;
+
+    @Autowired
+    private QualificationRepository qualificationRepository;
 
     @Autowired
     private ModelMapper modelMapper;
@@ -46,5 +59,34 @@ public class FellowshipCandidateServiceImpl implements IFellowshipCandidateServi
     public Response getCandidateCount() {
         long candidateCount = fellowshipCandidateRepository.count();
         return new Response(200, "Total no of candidates: " + candidateCount);
+    }
+
+    @Override
+    public Response updateCandidateBankInfo(CandidateBankDetailsDTO candidateBankDetailsDTO) {
+        candidateBankDetailsDTO.setIsAccountNumberVerified("Yes");
+        candidateBankDetailsDTO.setIsAdhaarNumVerified("Yes");
+        candidateBankDetailsDTO.setIsIfscCodeVerified("Yes");
+        candidateBankDetailsDTO.setIsPanNumberVerified("Yes");
+        candidateBankDetailsDTO.setCreatorStamp(LocalDateTime.now());
+        candidateBankDetailsDTO.setCreatorUser(candidateBankDetailsDTO.getName());
+        CandidateBankDetails candidateBankDetails = modelMapper.map(candidateBankDetailsDTO, CandidateBankDetails.class);
+        bankDetailsRepository.save(candidateBankDetails);
+        return new Response(200, "Bank Details Successfully Updated");
+    }
+
+    @Override
+    public Response updateQualificationDetails(CandidateQualificationDTO candidateQualificationDTO) {
+        candidateQualificationDTO.setIsDegreeNameVerified("Yes");
+        candidateQualificationDTO.setIsEmployeeDisciplinedVerified("Yes");
+        candidateQualificationDTO.setIsFinalYearPercentageVerified("Yes");
+        candidateQualificationDTO.setIsOtherTrainingVerified("Yes");
+        candidateQualificationDTO.setIsTrainingInstituteVerified("Yes");
+        candidateQualificationDTO.setIsPassingYearVerified("Yes");
+        candidateQualificationDTO.setIsTrainingDurationMonthVerified("Yes");
+        candidateQualificationDTO.setCreatorStamp(LocalDateTime.now());
+        candidateQualificationDTO.setCreatorUser("Admin");
+        CandidateQualification candidateQualification = modelMapper.map(candidateQualificationDTO,CandidateQualification.class);
+        qualificationRepository.save(candidateQualification);
+        return new Response(200, "Candidate Qualification Details Successfully Updated");
     }
 }
