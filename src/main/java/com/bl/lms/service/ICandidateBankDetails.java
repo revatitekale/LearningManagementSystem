5327a0ced0f269472b5b3af8f998ed68706222a2
unknown <revitekale1910@gmail.com>
2020-05-31 20:37:43 +0530

    MODIFIED:Chnaged end points name

--- src/main/java/com/bl/lms/service/ICandidateBankDetails.java
+++
@@ -1,8 +0,0 @@
-package com.bl.lms.service;
-
-import com.bl.lms.dto.CandidateBankDetailsDTO;
-import com.bl.lms.dto.Response;
-
-public interface ICandidateBankDetails {
-    Response updateCandidateBankInfo(CandidateBankDetailsDTO candidateBankDetailsDTO);
-}
--- src/main/java/com/bl/lms/service/ICandidateQualification.java
+++
@@ -1,8 +0,0 @@
-package com.bl.lms.service;
-
-import com.bl.lms.dto.CandidateQualificationDTO;
-import com.bl.lms.dto.Response;
-
-public interface ICandidateQualification {
-    Response updateQualificationDetails(CandidateQualificationDTO candidateQualificationDTO);
-}
