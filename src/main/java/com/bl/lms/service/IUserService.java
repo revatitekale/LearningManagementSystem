1cc8d7af22e679018e7ccfbd5b324956f5961589
unknown <revitekale1910@gmail.com>
2020-06-03 02:16:28 +0530

    MODIFIED:Changed code structure

--- src/main/java/com/bl/lms/service/IUserService.java
+++ src/main/java/com/bl/lms/service/IUserService.java
@@ -3,15 +3,17 @@ package com.bl.lms.service;
 import com.bl.lms.dto.LoginDTO;
 import com.bl.lms.dto.Response;
 import com.bl.lms.dto.UserDTO;
+import com.bl.lms.exception.LmsAppException;
+import com.bl.lms.model.User;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 
 @Service
 public interface IUserService {
-        Response register(UserDTO user);
 
-        Response sentEmail(String email) throws MessagingException;
-        boolean resetPassword(String password, String token);
+        User register(UserDTO user);
+        String getPasswordToken(String email) throws MessagingException, LmsAppException;
+        User resetPassword(String password, String token);
         String getAuthenticationToken(LoginDTO authenticationRequest) throws Exception;
 }
