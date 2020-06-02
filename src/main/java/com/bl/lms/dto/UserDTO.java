75d1ccf2d781a570d932a243f909b5a2930a8dda
unknown <revitekale1910@gmail.com>
2020-06-02 23:13:45 +0530

    MODIFIED:Annotations and regex patterns are added

--- src/main/java/com/bl/lms/dto/UserDTO.java
+++ src/main/java/com/bl/lms/dto/UserDTO.java
@@ -4,18 +4,27 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.*;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 public class UserDTO {
 
+    @NotNull
+    private long id;
+    @Pattern(regexp = "^[A-Z]+[A-Za-z0-9]+$")
     private String firstName;
+    @Pattern(regexp = "^[A-Z]+[A-Za-z0-9]+$")
     private String lastName;
+    @Email
     private String email;
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]*.{8,}$")
     private String password;
     private long contactNumber;
-    private String Verified;
+    private String verified;
+    @NotNull
     private LocalDateTime creatorStamp;
-    private String creatorUser;
+    private long creatorUser;
 
 }
