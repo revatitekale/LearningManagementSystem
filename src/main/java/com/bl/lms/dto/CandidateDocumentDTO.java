674913be9be5c88113b6f99fd46967d13da64126
unknown <revitekale1910@gmail.com>
2020-06-02 23:13:06 +0530

    MODIFIED:Corrected datatype

--- src/main/java/com/bl/lms/dto/CandidateDocumentDTO.java
+++ src/main/java/com/bl/lms/dto/CandidateDocumentDTO.java
@@ -1,30 +1,21 @@
 package com.bl.lms.dto;
 
-import com.bl.lms.model.FellowshipCandidate;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Getter(AccessLevel.PUBLIC)
 @Setter(AccessLevel.PUBLIC)
 public class CandidateDocumentDTO {
-    @NotNull
-    private FellowshipCandidate fellowshipCandidate;
-    @NotNull
+
+    private long id;
+    private long candidateId;
     private String documentType;
-    @NotNull
-    private String fileType;
-    @NotNull
-    private String documentName;
-    @NotNull
     private String documentPath;
-    @NotNull
-    @Builder.Default
     private String status;
-    @NotNull
-    @Builder.Default
-    private LocalDateTime creatorStamp = LocalDateTime.now();
-    @NotNull
-    private String creatorUser;
+    private LocalDateTime creatorStamp;
+    private long creatorUser;
+
 }
