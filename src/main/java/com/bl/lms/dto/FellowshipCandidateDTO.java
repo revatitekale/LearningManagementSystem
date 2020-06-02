759aa58da7b357840797b679d4a18073607cad64
unknown <revitekale1910@gmail.com>
2020-06-02 23:05:51 +0530

    MODIFIED:Changed data type

--- src/main/java/com/bl/lms/dto/FellowshipCandidateDTO.java
+++ src/main/java/com/bl/lms/dto/FellowshipCandidateDTO.java
@@ -2,15 +2,20 @@ package com.bl.lms.dto;
 
 import lombok.Data;
 
-import java.time.LocalDate;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
 public class FellowshipCandidateDTO {
+
+    @NotNull
     private long id;
     private String firstName;
     private String middleName;
     private String lastName;
+    @Email
     private String email;
     private String degree;
     private String hiredCity;
@@ -23,8 +28,9 @@ public class FellowshipCandidateDTO {
     private String knowledgeRemark;
     private String aggregateRemark;
     private String status;
-    private Date creatorStamp;
-    private String creatorUser;
+    private LocalDateTime creatorStamp;
+    private long creatorUser;
+    @NotNull
     private Date birthDate;
     private String isBirthDateVerified;
     private String parentOccupation;
@@ -33,7 +39,7 @@ public class FellowshipCandidateDTO {
     private String localAddress;
     private String permanentAddress;
     private String photoPath;
-    private LocalDate joiningDate;
+    private Date joiningDate;
     private String candidateStatus;
     private String documentStatus;
     private String remark;
