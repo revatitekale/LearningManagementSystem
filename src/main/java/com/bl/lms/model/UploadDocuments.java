02e787e31a16ddccadd9beb27d3fae1d532005bd
unknown <revitekale1910@gmail.com>
2020-06-02 23:26:42 +0530

    MODIFIED:Changed data type and added getter setter for creator stamp

--- src/main/java/com/bl/lms/model/UploadDocuments.java
+++ src/main/java/com/bl/lms/model/UploadDocuments.java
@@ -1,6 +1,9 @@
 package com.bl.lms.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Entity(name = "candidate_documents")
