44dbfd9c0cb958db7b5175825c47d880cfa3dc60
unknown <revitekale1910@gmail.com>
2020-06-03 02:15:38 +0530

    MODIFIED:Changed code structure

--- src/main/java/com/bl/lms/service/ICandidateService.java
+++ src/main/java/com/bl/lms/service/ICandidateService.java
@@ -4,25 +4,25 @@ import com.bl.lms.dto.CandidateDTO;
 import com.bl.lms.dto.Response;
 import com.bl.lms.exception.LmsAppException;
 import com.bl.lms.model.Candidate;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 
 public interface ICandidateService {
 
-    Response getHiredCandidate(MultipartFile filePath) throws IOException;
+    boolean getHiredCandidate(MultipartFile filePath) throws IOException;
 
-    void saveCandidateDetails(CandidateDTO sheetData) throws MessagingException;
+    void saveCandidateDetails(CandidateDTO candidateDTO) throws MessagingException;
+
+    void sendEmail(CandidateDTO candidateDTO) throws MessagingException;
 
     List getHiredCandidatesList();
 
-    Candidate showById(Long id);
+    Optional<Candidate> showById(Long id);
 
-    Response updateCandidateStatus(String response, String email) throws LmsAppException;
-
-    Response sendJobOffer() throws MessagingException;
+    Candidate updateCandidateStatus(String response, String email) throws LmsAppException;
 }
