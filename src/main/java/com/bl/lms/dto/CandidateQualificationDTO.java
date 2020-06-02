61ada69898c1e2b78c809c14b40a01452e40dba7
unknown <revitekale1910@gmail.com>
2020-06-02 23:03:07 +0530

    MODIFIED:Not null annotation is added

--- src/main/java/com/bl/lms/dto/CandidateQualificationDTO.java
+++ src/main/java/com/bl/lms/dto/CandidateQualificationDTO.java
@@ -1,25 +1,30 @@
 package com.bl.lms.dto;
 
 import lombok.Data;
-
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Data
 public class CandidateQualificationDTO {
 
     @NotNull
     private long id;
+    @NotNull
     private long candidateId;
+    @NotNull
     private String diploma;
+    @NotNull
     private String degreeName;
     private String isDegreeNameVerified;
+    @NotNull
     private String employeeDescipline;
     private String isEmployeeDisciplinedVerified;
+    @NotNull
     private long passingYear;
     private String isPassingYearVerified;
+    @NotNull
     private double aggregatePercentage;
+    @NotNull
     private double finalYearPercentage;
     private String isFinalYearPercentageVerified;
     private String trainingInstitute;
@@ -29,5 +34,5 @@ public class CandidateQualificationDTO {
     private String otherTraining;
     private String isOtherTrainingVerified;
     private LocalDateTime creatorStamp;
-    private String creatorUser;
+    private long creatorUser;
 }
