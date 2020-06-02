bfc38d8cc60d67250303025232433554693ad92e
unknown <revitekale1910@gmail.com>
2020-06-03 02:12:42 +0530

    REFACTORED:Add commit and made autowiring private

--- src/main/java/com/bl/lms/controller/UserController.java
+++ src/main/java/com/bl/lms/controller/UserController.java
@@ -1,46 +1,68 @@
 package com.bl.lms.controller;
 
+import com.bl.lms.configuration.ApplicationConfig;
 import com.bl.lms.dto.*;
+import com.bl.lms.exception.LmsAppException;
+import com.bl.lms.model.User;
 import com.bl.lms.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.mail.MessagingException;
+import javax.validation.Valid;
 
 @RestController
-@CrossOrigin
 @RequestMapping("/user")
 public class UserController {
 
     @Autowired
-    IUserService userService;
+    private IUserService userService;
 
+    /**
+     * @param userDTO
+     * @return response(Registered user)
+     */
+    @PostMapping("/register")
+    public ResponseEntity<Response> register(@RequestBody UserDTO userDTO) {
+        User userModel = userService.register(userDTO);
+        return new ResponseEntity<>(new Response(userModel, 200, ApplicationConfig.getMessageAccessor().getMessage("101")), HttpStatus.CREATED);
+
+    }
+
+    /**
+     * @param loginRequest
+     * @return response(token)
+     * @throws Exception
+     */
     @PostMapping("/authenticate")
     public ResponseEntity<ResponseJwt> createAuthenticationToken(@RequestBody LoginDTO loginRequest) throws Exception {
         String token = userService.getAuthenticationToken(loginRequest);
-        return ResponseEntity.ok(new ResponseJwt(token));
+        return new ResponseEntity<>(new ResponseJwt(token, ApplicationConfig.getMessageAccessor().getMessage("106")), HttpStatus.CREATED);
+
     }
 
-    @GetMapping("/homepage")
-    public String login() {
-        return "Welcome to Learner Management System";
-    }
-
-    @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody UserDTO user) throws Exception {
-        return ResponseEntity.ok(userService.register(user));
-    }
-
+    /**
+     * @param email
+     * @return passwordToken
+     * @throws MessagingException
+     * @throws LmsAppException
+     */
     @GetMapping("/forgetpassword")
-    public Response requestResetPassword(@RequestParam (value = "email") String email) throws MessagingException {
-        return userService.sentEmail(email);
+    public ResponseEntity<Response> requestResetPassword(@Valid @RequestParam (value = "email") String email) throws MessagingException, LmsAppException {
+        String passwordToken = userService.getPasswordToken(email);
+        return new ResponseEntity<>(new Response(passwordToken, 200, ApplicationConfig.getMessageAccessor().getMessage("103")), HttpStatus.ACCEPTED);
     }
 
+    /**
+     * @param resetPassword
+     * @return response(Reseted password)
+     */
     @PutMapping("/resetpassword")
-    public Response resetPassword(@RequestBody ForgetPasswordDTO resetPassword) {
-        boolean result = userService.resetPassword(resetPassword.getPassword(), resetPassword.getToken());
-        if (result)
-            return new Response(200, "Successfully updated");
+    public ResponseEntity<Response> resetPassword(@Valid @RequestBody ForgetPasswordDTO resetPassword) {
+        User user = userService.resetPassword(resetPassword.getPassword(), resetPassword.getToken());
+        if (!user.equals(null))
+            return new ResponseEntity<>(new Response(user, 200, ApplicationConfig.getMessageAccessor().getMessage("104")), HttpStatus.ACCEPTED);
         return null;
     }
 }
