7cfefd9fd2c9456e47c739ed61b9c6ddaea1b76c
unknown <revitekale1910@gmail.com>
2020-06-03 13:23:30 +0530

    MODIFIED:Add upload documents method

--- src/main/java/com/bl/lms/service/FellowshipCandidateServiceImpl.java
+++ src/main/java/com/bl/lms/service/FellowshipCandidateServiceImpl.java
@@ -1,30 +1,29 @@
 package com.bl.lms.service;
 
-import com.bl.lms.dto.CandidateBankDetailsDTO;
-import com.bl.lms.dto.CandidateQualificationDTO;
-import com.bl.lms.dto.FellowshipCandidateDTO;
-import com.bl.lms.dto.Response;
+import com.bl.lms.configuration.CloudinaryConfig;
+import com.bl.lms.dto.*;
 import com.bl.lms.exception.LmsAppException;
-import com.bl.lms.model.Candidate;
-import com.bl.lms.model.CandidateBankDetails;
-import com.bl.lms.model.CandidateQualification;
-import com.bl.lms.model.FellowshipCandidate;
-import com.bl.lms.repository.BankDetailsRepository;
-import com.bl.lms.repository.CandidateRepository;
-import com.bl.lms.repository.FellowshipCandidateRepository;
-import com.bl.lms.repository.QualificationRepository;
+import com.bl.lms.model.*;
+import com.bl.lms.repository.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Service
@@ -48,22 +47,38 @@ public class FellowshipCandidateServiceImpl implements IFellowshipCandidateServi
     @Autowired
     private JavaMailSender sender;
 
+    @Autowired
+    private CloudinaryConfig cloudinaryConfig;
+
+    @Autowired
+    private CandidateDocumentRepository candidateDocumentRepository;
+
+    /**
+     * @param id
+     * @return response(fellowshipCandidateModel)
+     */
     @Override
-    public FellowshipCandidate joinCandidateToFellowship(long id) {
+    public FellowshipCandidate joinCandidateToFellowship(long id) throws MessagingException {
         Candidate hiredCandidateModel = hiredCandidateRepository.findById(id)
                 .orElseThrow(() -> new LmsAppException(LmsAppException.exceptionType.INVALID_ID, "User not found"));
         FellowshipCandidate fellowshipCandidateModel = modelMapper.map(hiredCandidateModel, FellowshipCandidate.class);
-        if (fellowshipCandidateModel.equals(null))
-            throw new LmsAppException(LmsAppException.exceptionType.DATA_NOT_FOUND, "Null Value");
+        sendMail(fellowshipCandidateModel);
         return fellowshipCandidateRepository.save(fellowshipCandidateModel);
     }
 
+    /**
+     * @return Candidate count
+     */
     @Override
     public int getCandidateCount() {
         List<FellowshipCandidate> list = fellowshipCandidateRepository.findAll();
         return list.size();
     }
 
+    /**
+     * @param fellowshipCandidateDto
+     * @return Updated candidates personal information
+     */
     @Override
     public FellowshipCandidate updateInformation(FellowshipCandidateDTO fellowshipCandidateDto) {
         Candidate hiredCandidateModel = hiredCandidateRepository.findById(fellowshipCandidateDto.getId())
@@ -74,6 +89,11 @@ public class FellowshipCandidateServiceImpl implements IFellowshipCandidateServi
         return fellowshipCandidateRepository.save(fellowshipMappedCandidate);
     }
 
+    /**
+     * @param fellowshipCandidateModel
+     * @throws MessagingException
+     * response(Sent email to candidates)
+     */
     @Override
     public void sendMail(FellowshipCandidate fellowshipCandidateModel) throws MessagingException {
         String recipientAddress = fellowshipCandidateModel.getEmail();
@@ -87,25 +107,89 @@ public class FellowshipCandidateServiceImpl implements IFellowshipCandidateServi
                 + "Reject: " + "\n" + "http://localhost:8084/" +
                 "candidatehiring/status?response=Rejected&email=" + fellowshipCandidateModel.getEmail() + "\n\n");
         helper.setSubject("Job offer notification");
-        //sender.send(message);
     }
 
+    /**
+     * @param bankDetailsDto
+     * @return response(Updated bank details)
+     */
     @Override
     public CandidateBankDetails updateBankDetails(CandidateBankDetails bankDetailsDto) {
         fellowshipCandidateRepository.findById(bankDetailsDto.getCandidateId())
                 .orElseThrow(() -> new LmsAppException(LmsAppException.exceptionType.INVALID_ID, "Invalid Id"));
         CandidateBankDetails bankDetailsModel = modelMapper.map(bankDetailsDto, CandidateBankDetails.class);
-        bankDetailsModel.setCreatorStamp(new LocalDateTime());
-        bankDetailsModel.setCreatorUser(bankDetailsDto.getName());
         return bankDetailsRepository.save(bankDetailsModel);
     }
 
+    /**
+     * @param candidateQualificationDto
+     * @return Updated qualification details
+     */
     @Override
     public CandidateQualification updateQualificationDetails(CandidateQualificationDTO candidateQualificationDto) {
         fellowshipCandidateRepository.findById(candidateQualificationDto.getCandidateId())
                 .orElseThrow(() -> new LmsAppException(LmsAppException.exceptionType.INVALID_ID, "Invalid id"));
         CandidateQualification qualificationDetails = modelMapper.map(candidateQualificationDto, CandidateQualification.class);
-        qualificationDetails.setCreatorStamp(new Date());
         return qualificationRepository.save(qualificationDetails);
     }
+
+    /**
+     * @param file
+     * @param updateDocumentDto
+     */
+    @Override
+    public String uploadDocuments(MultipartFile file, String updateDocumentDto) {
+        try {
+            CandidateDocumentDTO uploadDocumentsDto = new ObjectMapper().readValue(updateDocumentDto, CandidateDocumentDTO.class);
+            fellowshipCandidateRepository.findById(uploadDocumentsDto.getId())
+                    .orElseThrow(() -> new LmsAppException(LmsAppException.exceptionType.DATA_NOT_FOUND, "Data not found"));
+            if (file.isEmpty())
+                throw new LmsAppException(LmsAppException.exceptionType.DATA_NOT_FOUND, "Failed to store empty file");
+            Map<Object, Object> parameters = new HashMap<>();
+            parameters.put("public_id", "CandidateDocuments/" + uploadDocumentsDto.getId() + "/" + file.getOriginalFilename());
+            File uploadedFile = convertMultiPartToFile(file);
+            Map uploadResult = cloudinaryConfig.cloudinaryConfig().uploader().upload(uploadedFile, parameters);
+            String url = uploadResult.get("url").toString();
+            uploadDocumentsDto.setDocumentPath(url);
+            CandidateDocuments uploadDocumentsDao = modelMapper.map(uploadDocumentsDto, CandidateDocuments.class);
+            candidateDocumentRepository.save(uploadDocumentsDao);
+            return url;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * @param file
+     * @param id
+     * @throws LmsAppException
+     * @throws IOException
+     */
+    @Override
+    public boolean upload(MultipartFile file, long id) throws LmsAppException, IOException {
+        fellowshipCandidateRepository.findById(id)
+                .orElseThrow(() -> new LmsAppException(LmsAppException.exceptionType.DATA_NOT_FOUND, "Data not found"));
+        if (file.isEmpty())
+            throw new LmsAppException(LmsAppException.exceptionType.DATA_NOT_FOUND, "Files are empty");
+        File convertFile = new File(id + "-" + file.getOriginalFilename());
+        convertFile.createNewFile();
+        FileOutputStream fileOutputStream = new FileOutputStream(convertFile);
+        fileOutputStream.write(file.getBytes());
+        fileOutputStream.close();
+        return false;
+    }
+
+    /**
+     * @param file
+     * @throws IOException
+     */
+    @Override
+    public File convertMultiPartToFile(MultipartFile file) throws IOException {
+        File convertFile = new File(file.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(convertFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convertFile;
+    }
+
 }
