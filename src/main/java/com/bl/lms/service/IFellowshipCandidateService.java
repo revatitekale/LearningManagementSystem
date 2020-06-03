13cd934e4f44864d750bd22b63691c219a3f1c80
unknown <revitekale1910@gmail.com>
2020-06-03 13:23:24 +0530

    MODIFIED:Add upload documents method

--- src/main/java/com/bl/lms/service/IFellowshipCandidateService.java
+++ src/main/java/com/bl/lms/service/IFellowshipCandidateService.java
@@ -4,18 +4,27 @@ import com.bl.lms.dto.CandidateBankDetailsDTO;
 import com.bl.lms.dto.CandidateQualificationDTO;
 import com.bl.lms.dto.FellowshipCandidateDTO;
 import com.bl.lms.dto.Response;
+import com.bl.lms.exception.LmsAppException;
 import com.bl.lms.model.CandidateBankDetails;
 import com.bl.lms.model.CandidateQualification;
 import com.bl.lms.model.FellowshipCandidate;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
+import java.io.File;
+import java.io.IOException;
 
 public interface IFellowshipCandidateService {
 
-    FellowshipCandidate joinCandidateToFellowship(long id);
+    FellowshipCandidate joinCandidateToFellowship(long id) throws MessagingException;
     int getCandidateCount();
     FellowshipCandidate updateInformation(FellowshipCandidateDTO fellowshipCandidateDto);
     void sendMail(FellowshipCandidate fellowshipCandidateModel) throws MessagingException;
     CandidateBankDetails updateBankDetails(CandidateBankDetails bankDetailsDto);
-    CandidateQualification updateQualificationDetails(CandidateQualificationDTO candidateQualificationDto)
+    CandidateQualification updateQualificationDetails(CandidateQualificationDTO candidateQualificationDto);
+    String uploadDocuments(MultipartFile file, String updateDocumentDto);
+
+    boolean upload(MultipartFile file, long id) throws LmsAppException, IOException;
+
+    File convertMultiPartToFile(MultipartFile file) throws IOException;
 }
