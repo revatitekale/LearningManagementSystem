29ae347e8f0c586e633ec9bd3a9f79301756909e
unknown <revitekale1910@gmail.com>
2020-06-08 01:00:14 +0530

    ADDED:Configuration for database

---
+++ src/main/java/com/bl/lms/configuration/DatabaseConfiguration.java
@@ -0,0 +1,21 @@
+package com.bl.lms.configuration;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(System.getenv().get("datasource.driver-class-name"));
+        dataSourceBuilder.url(System.getenv().get("datasource.url"));
+        dataSourceBuilder.username(System.getenv().get("datasource.username"));
+        dataSourceBuilder.password(System.getenv().get("datasource.password"));
+        return dataSourceBuilder.build();
+    }
+}
