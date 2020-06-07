a05244f8537b0c2933bf2ea34d296a678f453a1c
unknown <revitekale1910@gmail.com>
2020-06-08 01:00:34 +0530

    ADDED:Configuration for mail

---
+++ src/main/java/com/bl/lms/configuration/EmailConfiguration.java
@@ -0,0 +1,32 @@
+package com.bl.lms.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Map;
+import java.util.Properties;
+
+@Configuration
+public class EmailConfiguration {
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        Map<String, String> map = System.getenv();
+        String username = map.get("username");
+        String password = map.get("password");
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+        return mailSender;
+    }
+
+}
