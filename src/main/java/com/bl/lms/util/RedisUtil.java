246f0c385ee1220b4b715bdfcd39414bb89efef7
unknown <revitekale1910@gmail.com>
2020-06-11 10:08:23 +0530

    ADDED:Util class to save data in redis

---
+++ src/main/java/com/bl/lms/util/RedisUtil.java
@@ -0,0 +1,26 @@
+package com.bl.lms.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.Map;
+
+@Component
+public class RedisUtil implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    public void save(String RedisKey, String email, String authenticationToken) {
+        redisTemplate.opsForHash().put(RedisKey, email, authenticationToken);
+    }
+
+    public Map<Object, Object> getValue(String redisKey) {
+        return redisTemplate.opsForHash().entries(redisKey);
+    }
+
+}
